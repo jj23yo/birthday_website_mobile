6745e0b8a9af0257582cc08bb8cde29585625604
jj23yo <jess.jacq23@gmail.com>
2026-03-13 23:02:04 +0100

    Update text color and content in Picture component

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -19,8 +19,8 @@ function Picture() {
   return (
     <SectionWrapper>
       <Link to="/card">
-        <p className="absolute text-4xl font-bold text-customBlue inset-0 flex justify-center items-center text-center transform rotate-6 cursor-pointer">
-          You're Getting Old! :P
+        <p className="absolute text-4xl font-bold text-[#FDF0D5] inset-0 flex justify-center items-center text-center transform rotate-6 cursor-pointer">
+          You're Getting Old! Ehehe :P
         </p>
       </Link>
       {!allImagesLoaded && (
