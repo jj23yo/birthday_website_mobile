11f6a47d1d84464a7974051c60ab5173e987bce0
jj23yo <jess.jacq23@gmail.com>
2026-03-15 11:10:39 +0100

    Update ticket text in Present component

--- src/components/Present.jsx
+++ src/components/Present.jsx
@@ -16,17 +16,17 @@ function Present() {
           <div className="gift-bottom"></div>
           <div id="raffle-red" className="entry raffle raffle-1">
             <div className="no-scale">
-              <span className="ticket-text">Free<br/>wish (non horny duh)</span>
+              <span className="ticket-text">Wish<br/>(non horny duh)</span>
             </div>
           </div>
           <div id="raffle-red" className="entry raffle raffle-2">
             <div className="no-scale">
-              <span className="ticket-text">Free<br/>Pokemon card</span>
+              <span className="ticket-text">Free<br/>Me</span>
             </div>
           </div>
           <div id="raffle-red" className="entry raffle raffle-3">
             <div className="no-scale">
-              <span className="ticket-text">Free<br/>Me</span>
+              <span className="ticket-text">Free<br/>Hug</span>
             </div>
           </div>
         </label>
